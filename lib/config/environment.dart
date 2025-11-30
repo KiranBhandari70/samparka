c43afc44e3870ce7bd6d0ff8880c43c8dc73ae46
KiranBhandari70 <kiran.bhandariofficial70@gmail.com>
2025-11-30 09:40:20 +0545

    final commit

--- lib/config/environment.dart
+++ lib/config/environment.dart
@@ -4,7 +4,7 @@ class Environment {
   // For local development, use: http://localhost:5000
   static const String apiBaseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue:'http://192.168.1.166:5000',
+    defaultValue:'https://samparka-1.onrender.com',
   );
 
   static bool get isProduction => apiBaseUrl.contains('samparka-n7ps.onrender.com');
